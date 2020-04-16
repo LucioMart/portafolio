8b29ae9d939e5948667004035932e1c78202a94e
LucioMart <luchoweb15@gmail.com>
2020-04-16 16:50:26 -0300

    Agregando repaso de rutas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+
+Route::get('/', function(){
+    return "Retomando el Estudio de Laravel despues de varios dias";
+});
+
+Route::get('/saludos/{nombre?}', function($nombre = ""){
+    return "Bienvenido " . $nombre;
+});
+
+Route::get('/contactame', function(){
+    return 'Seccion de contactos';
+})->name('contactos');
+
+Route::get('/home', function() {
+    echo '<a href="'.route('contactos').'">Contactos 1</a><br>';
+    echo '<a href="'.route('contactos').'">Contactos 2</a><br>';
+    echo '<a href="'.route('contactos').'">Contactos 3</a><br>';
+    echo '<a href="'.route('contactos').'">Contactos 4</a><br>';
+    echo '<a href="'.route('contactos').'">Contactos 5</a>';
 });

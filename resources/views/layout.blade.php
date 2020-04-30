1a25f4c1bc15e0c0d5aedb180e734108cd22a234
LucioMart <luchoweb15@gmail.com>
2020-04-30 18:22:54 -0300

    maquetacion del navb$

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix("/css/app.css") }}">
     <script src="/js/app.js" defer></script>
     <title>@yield('tituloPagina')</title>
@@ -15,12 +16,16 @@
 </head>
 <body>
    {{-- <pre> {{ dump(request()->routeIs('home')) }} </pre>  --}}
-    
-    @include('nav.navb')
 
-    <h1>@yield('tituloEncabezado')</h1>
-    <section>
-        @yield('content')
-    </section>
+   <div id="app">
+    
+        @include('nav.navb')
+
+        <h1>@yield('tituloEncabezado')</h1>
+        <section>
+            @yield('content')
+        </section>
+        
+   </div>
 </body>
 </html>
